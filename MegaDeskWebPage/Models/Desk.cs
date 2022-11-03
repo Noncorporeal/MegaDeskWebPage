c48e66abbc942cc6d7c28a36a19e8484e96783b7
Noncorporeal <42417289+Noncorporeal@users.noreply.github.com>
2022-11-03 08:28:21 -0600

    Copied Desk classes

---
+++ MegaDeskWebPage/Models/Desk.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace MegaDeskWebPage.Models
+{
+    
+    public enum DeskMaterials
+    {
+        Oak = 200,
+        Laminate = 100,
+        Pine = 50,
+        Rosewood = 300,
+        Veneer = 125
+    }
+
+    public class Desk
+    {
+        public static readonly int maxWidth = 96;
+        public static readonly int minWidth = 24;
+        public static readonly int maxDepth = 48;
+        public static readonly int minDepth = 12;
+        public static readonly int minDrawers = 0;
+        public static readonly int maxDrawers = 7;
+
+        public decimal Depth { get; set; }
+        public decimal Width { get; set; }
+        public DeskMaterials DeskMaterial { get; set; }
+        public int NumberOfDrawers { get; set; }
+
+        public Desk(
+            decimal depth,
+            decimal width,
+            DeskMaterials deskMaterial,
+            int numberOfDrawers)
+        {
+            Depth = depth;
+            Width = width;
+            DeskMaterial = deskMaterial;
+            NumberOfDrawers = numberOfDrawers;
+        }
+
+        public decimal GetArea()
+        {
+            return Depth * Width;
+        }
+    }
+}
+
---
+++ MegaDeskWebPage/Models/DeskQuote.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+namespace MegaDeskWebPage.Models
+{
+    namespace MegaDesk_Ebenal
+    {
+        public enum DeliveryType
+        {
+            [Description("3 Day Shipping")]
+            ThreeDay,
+            [Description("5 Day Shipping")]
+            FiveDay,
+            [Description("7 Day Shipping")]
+            SevenDay,
+            [Description("Standard Shipping")]
+            FourteenDay
+        }
+
+        public class DeskQuote
+        {
+            public DateTime QuoteDate { get; set; }
+            public string CustomerName { get; set; }
+            public DeliveryType DeliveryType { get; set; }
+            public decimal QuoteAmount { get; set; }
+            public Desk Desk { get; set; }
+
+            [JsonConstructor]
+            public DeskQuote(
+                string customerName,
+                DeliveryType deliveryType,
+                Desk desk,
+                decimal quoteAmount,
+                DateTime quoteDate)
+            {
+                QuoteDate = quoteDate;
+                CustomerName = customerName;
+                DeliveryType = deliveryType;
+                QuoteAmount = quoteAmount;
+                Desk = desk;
+            }
+
+            public DeskQuote(
+                string customerName,
+                DeliveryType deliveryType,
+                Desk desk)
+            {
+                QuoteDate = DateTime.Now;
+                CustomerName = customerName;
+                DeliveryType = deliveryType;
+                Desk = desk;
+                QuoteAmount = GetQuotePrice();
+            }
+
+            public decimal GetQuotePrice()
+            {
+                if (!File.Exists(@"./rushOrderPrices.txt"))
+                {
+                    return 0;
+                }
+
+                //List<List<decimal>> rushPrices = File.ReadAllLines(@"./rushOrderPrices.txt").Select((str, index) => new { index, number = decimal.Parse(str) }).ToLookup(_ => _.index / 3, _ => _.number).Select(_ => _.ToList()).ToList();
+                List<(int index, string value)> fileLines = File.ReadAllLines(@"./rushOrderPrices.txt")
+                    .Select((value, index) => (index, value)).ToList();
+                List<List<decimal>> rushPrices =
+                    (from price in fileLines
+                     group decimal.Parse(price.value)
+                     by price.index / 3
+                     into prices
+                     select prices
+                     .ToList())
+                     .ToList();
+
+                decimal totalPrice = 200
+                    + Desk.NumberOfDrawers * 50
+                    + (Desk.GetArea() > 1000 ? Desk.GetArea() - 1000 : 0)
+                    + (int)Desk.DeskMaterial;
+
+                int idx = (int)Desk.GetArea() % 1000;
+                totalPrice += (int)DeliveryType <= 2 ? rushPrices[(int)DeliveryType][idx <= 2 ? idx : 2] : 0;
+                return totalPrice;
+            }
+
+            public string[] Stringify()
+            {
+                string[] stringQuote =
+                {
+                QuoteDate.ToString(),
+                CustomerName,
+                Desk.Width.ToString(),
+                Desk.Depth.ToString(),
+                Desk.NumberOfDrawers.ToString(),
+                Desk.DeskMaterial.ToString(),
+                DeliveryString(),
+                QuoteAmount.ToString(),
+            };
+                return stringQuote;
+            }
+
+            private string DeliveryString()
+            {
+                switch (DeliveryType)
+                {
+                    case DeliveryType.FourteenDay:
+                        return "14 Days";
+                    case DeliveryType.SevenDay:
+                        return "7 Days";
+                    case DeliveryType.FiveDay:
+                        return "5 days";
+                    case DeliveryType.ThreeDay:
+                        return "3 Days";
+                }
+                return "Not Specified";
+            }
+        }
+
